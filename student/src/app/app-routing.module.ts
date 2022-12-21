3bf5884fd94071afcc8459cd08f46e912179345e
Kulasekaralwar Chandiran <kulasekaralwar.c@gmail.com>
2022-12-21 20:01:18 +0530

    latest changes

--- student/src/app/app-routing.module.ts
+++ student/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { AboutUsComponent } from './about-us/about-us.component';
+import { AuthComponent } from './auth/auth.component';
+import { NotFoundComponent } from './shared/components/not-found/not-found.component';
+
+const routes: Routes = [
+  { path:'about-us',component:AboutUsComponent},
+
+  {path:'auth',component:AuthComponent,
+  loadChildren:()=> import('./auth/auth.module').then(x=>x.AuthModule)},
+
+  { path:'**',component:NotFoundComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
